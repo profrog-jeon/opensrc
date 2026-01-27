06264bcc2a9e9f2d7a9c0519325255ba1e00f0fc
전상현 <sean.jeon@akdan.co.kr>
2026-01-27 23:27:39 +0900

    pcre2 라이브러리 소스코드 및 윈도우 빌드 설정 추가

---
+++ Test/pcre2Test/pch.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define PCRE2_CODE_UNIT_WIDTH 16  // 보통 UTF-8 환경에서는 8을 사용
+
+#include "../../Src/pcre2/config.h"
+#include "../../Src/pcre2/pcre2.h"
+#include "../../../cppcore/Inc/cppcore.h"
+
+#pragma comment(lib, "cppcore.lib")
