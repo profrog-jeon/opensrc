ee7d481fcbcbdea0652302ad58f384944644c660
전상현 <sean.jeon@akdan.co.kr>
2026-07-29 14:39:14 +0900

    yara 소스코드가 리눅스 빌드 이후에 나타나므로 빌드 순서를 리눅스 -> 윈도우로 변경

--- Build/Build.bat
+++ Build/Build.bat
@@ -1,16 +1,5 @@
 @ECHO OFF
 
-REM =======================================
-REM ===========  WINDOWS BUILD  ===========
-REM =======================================
-SET MSBUILD="C:\Program Files\Microsoft Visual Studio\18\Community\MSBuild\Current\Bin\MSBuild.exe"
-SET SLNFILE=..\Src\_BuildAll.sln
-
-%MSBUILD% %SLNFILE% /m /t:Rebuild /p:Configuration=ReleaseMT;Platform=x64
-IF %ERRORLEVEL% NEQ 0 GOTO Error
-%MSBUILD% %SLNFILE% /m /t:Rebuild /p:Configuration=Release;Platform=x64
-IF %ERRORLEVEL% NEQ 0 GOTO Error
-
 REM =====================================
 REM ===========  LINUX BUILD  ===========
 REM =====================================
@@ -31,6 +20,17 @@ cd ..\Output\libarchive
 wsl cmake ../../Src/libarchive
 IF %ERRORLEVEL% NEQ 0 GOTO Error
 wsl cmake --build . --parallel 16
+IF %ERRORLEVEL% NEQ 0 GOTO Error
+
+REM =======================================
+REM ===========  WINDOWS BUILD  ===========
+REM =======================================
+SET MSBUILD="C:\Program Files\Microsoft Visual Studio\18\Community\MSBuild\Current\Bin\MSBuild.exe"
+SET SLNFILE=..\Src\_BuildAll.sln
+
+%MSBUILD% %SLNFILE% /m /t:Rebuild /p:Configuration=ReleaseMT;Platform=x64
+IF %ERRORLEVEL% NEQ 0 GOTO Error
+%MSBUILD% %SLNFILE% /m /t:Rebuild /p:Configuration=Release;Platform=x64
 IF %ERRORLEVEL% NEQ 0 GOTO Error
 
 REM ===============================
